7caa5bfec1cf37f677a5d505248ada6f0790cf90
Owen Wong <owenwong@uw.edu>
2026-02-07 23:12:31 -0800

    Initial Commit

---
+++ ikidTests/ikidTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikidTests.swift
+//  ikidTests
+//
+//  Created by Owen Wong on 2/7/26.
+//
+
+import Testing
+@testable import ikid
+
+struct ikidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
