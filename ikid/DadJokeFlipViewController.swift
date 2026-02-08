deb829aa1b21183c05ebf62252dbe6a171bc687d
Owen Wong <owenwong@uw.edu>
2026-02-08 14:10:01 -0800

    Completed functionality

---
+++ ikid/DadJokeFlipViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DadJokeFlipViewController.swift
+//  ikid
+//
+//  Created by Owen Wong on 2/8/26.
+//
+
+import UIKit
+
+class DadJokeFlipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dissmiss(_ sender: Any) {
+        dismiss(animated: true , completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ikid/GoodJokeFlipViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GoodJokeFlipViewController.swift
+//  ikid
+//
+//  Created by Owen Wong on 2/8/26.
+//
+
+import UIKit
+
+class GoodJokeFlipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
